9547630eb90bea9552e441ca38e88cda2ed807b4
leahhartwell <Hartwell022@gmail.com>
2021-01-02 20:23:34 -0800

    setting up tfrecords

--- generate_tfrecord.py
+++ generate_tfrecord.py
@@ -2,10 +2,9 @@
 Usage:
   # From tensorflow/models/
   # Create train data:
-  python generate_tfrecord.py --csv_input=images/train_labels.csv --image_dir=images/train --output_path=train.record
-
+  python generate_tfrecord.py --csv_input=3Dprint_data/train_3Dprint_labels.csv  --image_dir=3Dprint_data/images --output_path=3Dprint_data/3Dprint_train.record
   # Create test data:
-  python generate_tfrecord.py --csv_input=images/test_labels.csv  --image_dir=images/test --output_path=test.record
+  python generate_tfrecord.py --csv_input=3Dprint_data/test_3Dprint_labels.csv --image_dir=3Dprint_data/images --output_path=3Dprint_data/3Dprint_test.record
 """
 from __future__ import division
 from __future__ import print_function
@@ -14,7 +13,7 @@ from __future__ import absolute_import
 import os
 import io
 import pandas as pd
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 
 from PIL import Image
 from object_detection.utils import dataset_util
@@ -22,45 +21,15 @@ from collections import namedtuple, OrderedDict
 
 flags = tf.app.flags
 flags.DEFINE_string('csv_input', '', 'Path to the CSV input')
-flags.DEFINE_string('image_dir', '', 'Path to the image directory')
+flags.DEFINE_string('image_dir', '', 'Path to images')
 flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
 FLAGS = flags.FLAGS
 
 
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
-    if row_label == 'random extrusion':
+    if row_label == 'fail':
         return 1
-    elif row_label == 'detached':
-        return 2
-    elif row_label == 'thin first layer':
-        return 3
-    elif row_label == 'random abnormality':
-        return 4
-    elif row_label == 'OK':
-        return 5
-    elif row_label == 'delamination':
-        return 6
-    elif row_label == 'layer shift':
-        return 7
-    elif row_label == 'over extrusion':
-        return 8
-    elif row_label == 'pillowing':
-        return 9
-    elif row_label == 'melted':
-        return 10
-    elif row_label == 'bad overhang':
-        return 11
-    elif row_label == 'messy first layer':
-        return 12
-    elif row_label == 'stringing':
-        return 13
-    elif row_label == 'under extrusion':
-        return 14    
-    elif row_label == 'warping':
-        return 15
-    elif row_label == 'gaps':
-        return 16
     else:
         None
 
@@ -114,7 +83,7 @@ def create_tf_example(group, path):
 
 def main(_):
     writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
-    path = os.path.join(os.getcwd(), FLAGS.image_dir)
+    path = os.path.join(FLAGS.image_dir)
     examples = pd.read_csv(FLAGS.csv_input)
     grouped = split(examples, 'filename')
     for group in grouped:
